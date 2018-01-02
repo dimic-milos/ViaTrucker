ec4f2bc6ffbb8c6ef2d52fa3ca2134109e1f1ee3
Milos Dimic <dimic.milos@icloud.com>
2018-01-02 13:30:45 +0100

    Initial Commit

---
+++ viaTruckerGit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  viaTruckerGit
+//
+//  Created by Dimic Milos on 1/2/18.
+//  Copyright © 2018 Dimic Milos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
